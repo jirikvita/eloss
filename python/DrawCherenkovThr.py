b3f76dfa0c4631e9b25673c917ab077c5a32e980
Jiri Kvita <jiri.kvita@gmail.com>
2023-06-24 15:20:43 +0200

    graphics

--- python/DrawCherenkovThr.py
+++ python/DrawCherenkovThr.py
@@ -3,38 +3,46 @@
 # jk 24.6.2023
 
 import ROOT
-
 from math import *
-
 from collections import OrderedDict
 
 Particles = OrderedDict({ #'e': 0.511,
-                          'mu' : 105.7, 'pi' : 139.6,
-                          #'p' : 938, 'K' : 493.7
+                          'mu' : 105.7,
+                          'pi' : 139.6,
+                          #'K' : 493.7,
+                          #'p' : 938,
                          }
                         ) # MeV
 
 cols = OrderedDict({ 'e': ROOT.kRed,
                      'mu' : ROOT.kBlue,
                      'pi' : ROOT.kGreen+1,
+                     'K' : ROOT.kMagenta,
                      'p' : ROOT.kBlack,
-                     'K' : ROOT.kMagenta
                     }
                    )
 
 plabels = OrderedDict({ 'e': 'e',
                         'mu' : '#mu',
                         'pi' : '#pi',
+                        'K' : 'K',                
                         'p' : 'p',
-                        'K' : 'K'
                        }
                       )
 
+
+ns = [#1.006, 1.01,
+    #1.01,
+    1.06, 1.10, 1.15]
+
+cans = []
 
 # momentum:
 Momenta = [220, 240, 260, 280, 300, 320, 340, 360, 
            #400, 500, 600, 700, 800, 900, 1000,
            ] # MeV
+p0 = Momenta[0]
+p1 = Momenta[-1]
 
 line = ''
 for momentum in Momenta:
@@ -49,13 +57,13 @@ for momentum in Momenta:
     print(betas)
 
 
-p0 = Momenta[0]
-p1 = Momenta[-1]
-
 # beta(p|m)
 funs_betap = OrderedDict()
 
-
+canname = 'CherenkovBetaPlot'
+cw, ch = 800, 600
+can1 = ROOT.TCanvas(canname, canname, 0, 0, cw, ch)
+cans.append(can1)
 for particle in Particles:
     mass = Particles[particle]
     name = f'fun_beta_{particle}'
@@ -65,7 +73,9 @@ for particle in Particles:
     fun.SetNpx(1000)
     fun.SetParameters(mass)
     funs_betap[particle] = fun
-    
+leg = ROOT.TLegend(0.75, 0.15, 0.90, 0.35)
+leg.SetBorderSize(0)
+
 opt = 'same'
 nbx, nby = 100, 100
 h2 = ROOT.TH2D('tmp', 'tmp;p [MeV/c];#beta', nbx, p0, p1, nby, 0., 1.1)
@@ -76,14 +86,16 @@ ROOT.gStyle.SetOptTitle(0)
 for particle,fun in funs_betap.items():
     fun.SetLineColor(cols[particle])
     fun.Draw('same')
+    leg.AddEntry(fun, f'{plabels[particle]}', 'L')
+leg.Draw()
 ROOT.gPad.Update()
 
 
-ns = [#1.006, 1.01,
-    1.06, 1.10, 1.15]
 Funs_yieldsfp = OrderedDict()
-can = ROOT.TCanvas()
-can.Divide(2,2)
+canname = 'CherenkovIntensityPlot'
+cw, ch = 1000, 800
+can2 = ROOT.TCanvas(canname, canname, 200, 200, cw, ch)
+can2.Divide(2,2)
 jn = 0
 labels = OrderedDict()
 legs = OrderedDict()
@@ -105,9 +117,9 @@ for n in ns:
         funs_yieldsfp[particle] = fun
     Funs_yieldsfp[n] = funs_yieldsfp
     jn = jn + 1
-    can.cd(jn)
+    can2.cd(jn)
     h4 = h2.DrawCopy()
-    h4.GetYaxis().SetTitle('I ~ 1 - 1/(#betan)^{2}')
+    h4.GetYaxis().SetTitle('~ I ~ 1 - 1/(#betan)^{2}')
     h4.GetYaxis().SetRangeUser(0, 0.2)
     for particle,fun in funs_yieldsfp.items():
         fun.SetLineColor(cols[particle])
@@ -115,7 +127,13 @@ for n in ns:
         legs[n].AddEntry(fun, f'{plabels[particle]}', 'L')
     labels[n].Draw()
     legs[n].Draw()
-can.Update()
+    ROOT.gPad.SetGridx()
+    ROOT.gPad.SetGridy()
+can2.Update()
+cans.append(can2)
 
+for can in cans:
+    can.Print(can.GetName() + '.png')
+    can.Print(can.GetName() + '.pdf')
 
 ROOT.gApplication.Run()
